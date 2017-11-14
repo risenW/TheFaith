b7bcd3f58d8b08ef56fa4efbb6c1e8819cc3789e
Rising Odegua <williamsrising592@gmail.com>
2017-11-15 00:04:06 +0100

    Modified the Navigation Bar and added Header, profile pic and cover images

--- app/src/main/java/com/afridevelopers/thefaith/MainActivity.java
+++ app/src/main/java/com/afridevelopers/thefaith/MainActivity.java
@@ -1,13 +1,30 @@
 package com.afridevelopers.thefaith;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navigation);
+        setNavigationDrawer();
+    }
+
+    private void setNavigationDrawer() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/afridevelopers/thefaith/SplashActivity.java
+++ app/src/main/java/com/afridevelopers/thefaith/SplashActivity.java
@@ -1,13 +1,26 @@
 package com.afridevelopers.thefaith;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        button = (Button)findViewById(R.id.click);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
