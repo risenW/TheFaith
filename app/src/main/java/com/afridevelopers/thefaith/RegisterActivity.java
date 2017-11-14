c6382a69eb08679dcdab8f040383192302e12855
Rising Odegua <williamsrising592@gmail.com>
2017-11-14 20:33:33 +0100

    Added UI design to the Login Activity

--- app/src/main/java/com/afridevelopers/thefaith/RegisterActivity.java
+++ app/src/main/java/com/afridevelopers/thefaith/RegisterActivity.java
@@ -3,11 +3,11 @@ package com.afridevelopers.thefaith;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
